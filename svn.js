d1e8c4d4058a878ed2f6b0852d5cc92cb41f598e
Xiao Deng <xiao.deng@garmin.com>
2016-01-26 09:28:06 -0600

    SVN exit codes are unreliable. Detect error result by checking for empty stderr, credit github.com/jrsnider.

--- svn.js
+++ svn.js
@@ -249,6 +249,7 @@ function _process(args, cb) {
   child.on('exit', function childExit(code, sig) {
     process.removeListener('exit', parentExit); 
     if(code === 0) {
+    if(stderr.length === 0) {
       // console.log(stdout);
       if(args.indexOf('--xml') > -1) {
         xml2js.parseString(stdout, {
